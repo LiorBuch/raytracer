c2fb32ca873b178f6434dca0637c737824cd5386
LiorBuch <lior.buchmeier@gmail.com>
2024-08-09 12:46:08 +0300

    fix: cleaned out numba

--- batched_scene_builder.py
+++ batched_scene_builder.py
@@ -6,12 +6,7 @@ from ray import Ray
 from scene_settings import SceneSettings
 import concurrent.futures
 import multiprocessing as mp
-from surfaces.cube import Cube
-from surfaces.infinite_plane import InfinitePlane
-from surfaces.sphere import Sphere
 from surfaces.shape import Shape
-
-from numba import cuda
 from light import Light
 from material import Material
 
